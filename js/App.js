38c5f0445a63819335ad2d27f414ac310329c550
web.ljg <web.ljg@gmail.com>
2022-08-23 10:39:14 +0900

    js 모듈화

---
+++ js/App.js
@@ -0,0 +1,9 @@
+import SearchInput from './SearchInput.js';
+
+export default function App({ $target }) {
+  this.state = {};
+
+  this.setState = (nextState) => {};
+
+  const searchInput = new SearchInput({ $target, initialState: '' });
+}
---
+++ js/SearchInput.js
@@ -0,0 +1,20 @@
+export default function SearchInput({ $target, initialState }) {
+  this.$element = document.createElement('form');
+  this.$element.className = 'SearchInput';
+  this.state = initialState;
+
+  $target.appendChild(this.$element);
+
+  this.render = () => {
+    this.$element.innerHTML = `
+    <input
+      class="SearchInput__input"
+      type="text"
+      placeholder="프로그램 언어를 입력하세요."
+      value="Script"
+    />
+    `;
+  };
+
+  this.render();
+}
